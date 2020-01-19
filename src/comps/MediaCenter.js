128174fda14517fc64b70b689c5fb20d7cb09b03
Charlie <porthose.cjsmo@gmail.com>
2020-01-18 21:20:10 -0800

    corrected typos

--- src/comps/MediaCenter.js
+++ src/comps/MediaCenter.js
@@ -7,24 +7,24 @@ const MovieGo1Txt = ` was written in `
 
 const MovieGo2Txt = ` and is primarily meant to be used on the `
 
-const MovieGo3Txt = `.  MovieCenter was written to solve three
+const MovieGo3Txt = `.  MediaCenter was written to solve three
 main problems.  Problem #1 is the inherent limited storage capability
 of the Raspberry Pi (64GB).  This is unacceptable as I have 8TB of video
 and audio file to organize and serve up so some type of streaming solution
-is needed.  Problem #2 the crappy video playback from most if not all the video
-players CPU based players out in the wild.  `
+is needed.  Problem #2 the crappy video playback from most if not all CPU
+based players out in the wild.  `
 
 const MovieGo4Txt = `  is a GPU video player that was written in
-C++ especially for the Raspberry Pis  Broadcoms VideoCore chipset with near DVD
+C++ especially for the Raspberry Pi's  Broadcom VideoCore chipset with near DVD
 video quality playback in my opinion. Problem #3 my poor TV is dumber than a box
-of rocks and has no smart capabilities this is where the Raspberry Pi comes in.
+of rocks and has no smart capabilities, this is where the Raspberry Pi comes in.
 It is a cheap way of turning my dumb TV into a "smarter" one.  MediaCenter's is
 a natural progression of MovieGo and TvGo and is a mashup of both projects.  
 The front end was developed with HTML5, Bootstrap4, JQuery.  While `
 
 const MovieGo5Txt = `  and GoLang organizes and serves up your media files.  
 MovieGo can be accessed from any device that has a web browser, I find that an
-old phone can be used as a MovieGo remote control.`
+old phone can be used as a MediaCenter remote control.`
 
 function MediaCenter() {
     return(
